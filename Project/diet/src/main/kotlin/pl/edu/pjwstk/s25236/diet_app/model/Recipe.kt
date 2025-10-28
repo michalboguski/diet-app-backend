f982bf9650d016cc03b192fd9ec25b63a84ac412
Michal Boguski <s25236@pjwstk.edu.pl>
2025-10-28 23:13:14 +0000

    C4 reformat, ustawienia IDE, dodanie tabeli nutrient

--- Project/diet/src/main/kotlin/pl/edu/pjwstk/s25236/diet_app/model/Recipe.kt
+++ Project/diet/src/main/kotlin/pl/edu/pjwstk/s25236/diet_app/model/Recipe.kt
@@ -1,10 +1,8 @@
-package pl.edu.pjwstk.s25236.diet_app.pl.edu.pjwstk.s25236.diet_generator.model
-
-import pl.edu.pjwstk.s25236.diet_app.model.Ingredient
-import pl.edu.pjwstk.s25236.diet_app.model.Step
+package pl.edu.pjwstk.s25236.diet_app.model
 
 class Recipe(
     val id: Long,
     val ingredients: Map<Ingredient, Double>,
-    val steps: List<Step>) {
+    val steps: List<Step>
+) {
 }
