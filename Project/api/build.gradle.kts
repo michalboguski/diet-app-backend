1cc3e85152ac81c865112dc77956a43dbb71968e
Michal Boguski <s25236@pjwstk.edu.pl>
2025-10-30 22:41:16 +0000

    C5 Utworzenie Pierwszego kontrolere i dodanie potrzebnych zależności

--- Project/api/build.gradle.kts
+++ Project/api/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     kotlin("jvm")
-    id("org.openapi.generator") version "7.0.0"
+    id("org.openapi.generator") version "7.14.0"
 }
 
 dependencies {
     implementation(project(":diet"))
     testImplementation(kotlin("test"))
+
+    implementation(platform(libs.spring.boot.bom))
+    implementation(libs.spring.web)
+    implementation(libs.spring.context)
+    implementation(libs.swagger.models)
+    implementation(libs.jakarta.annotation)
+    implementation(libs.jakarta.validation)
+    implementation(libs.jakarta.servlet)
+    compileOnly(libs.swagger.annotations)
 }
 
 openApiGenerate {
@@ -18,7 +27,9 @@ openApiGenerate {
     additionalProperties.set(
         mapOf(
             "modelNameSuffix" to "Dto",
-            "useTags" to "true"
+            "interfaceOnly" to "true",
+            "apiVisibility" to "public",
+            "useTags" to "true",
         )
     )
 
@@ -30,6 +41,27 @@ openApiGenerate {
         )
     )
 }
+
+kotlin {
+    sourceSets {
+        main {
+            kotlin.srcDir(layout.buildDirectory.dir("generated/openapi/src/main/kotlin"))
+        }
+    }
+}
+
+//tasks.openApiGenerate {
+//    doLast {
+//        val springDocConfig = layout.buildDirectory
+//            .dir("generated/openapi/src/main/kotlin/org/openapitools")
+//            .get()
+//            .file("SpringDocConfiguration.kt")
+//            .asFile
+//        if (springDocConfig.exists()) {
+//            springDocConfig.delete()
+//        }
+//    }
+//}
 
 tasks.compileKotlin {
     dependsOn(tasks.openApiGenerate)
