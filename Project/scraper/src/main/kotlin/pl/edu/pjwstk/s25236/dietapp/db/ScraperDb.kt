c4d71fafcefae813b066679cb69247f0917482a0
Michał Boguski <michalboguski@gmail.com>
2026-09-20 15:59:45 +0200

    Usprawnienia i poprawki dopasowywania składników

--- Project/scraper/src/main/kotlin/pl/edu/pjwstk/s25236/dietapp/db/ScraperDb.kt
+++ Project/scraper/src/main/kotlin/pl/edu/pjwstk/s25236/dietapp/db/ScraperDb.kt
@@ -7,8 +7,8 @@ object ScraperDb {
     val dataSource: DataSource =
         HikariDataSource().apply {
             this.jdbcUrl = "jdbc:postgresql://146.59.95.136:5432/diet_db"
-            this.username = "scraper_user"
-            this.password = "1234"
+            this.username = "diet-generator-agent"
+            this.password = "agent123"
             this.maximumPoolSize = 3
             this.schema = "diet_app"
         }
